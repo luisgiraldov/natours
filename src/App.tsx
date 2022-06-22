bc164361005185e9bc28ffdee2190ca0be6b9e08
Luis Giraldo <luisgiraldo@Luiss-MacBook-Pro.local>
2022-06-22 15:45:24 -0400

    Added BEM architecture

--- src/App.tsx
+++ src/App.tsx
@@ -7,15 +7,15 @@ function App() {
   return (
     <div className="App">
       <header className="header">
-        <div className="logo-box">
-          <img src={logo} alt="logo" className="logo" />
+        <div className="header__logo-box">
+          <img src={logo} alt="logo" className="header__logo" />
         </div>
-        <div className="text-box">
+        <div className="header__text-box">
           <h1 className="heading-primary">
-            <div className="heading-primary-main">outdoors</div>
-            <div className="heading-primary-sub">is where life happens</div>
+            <div className="heading-primary--main">outdoors</div>
+            <div className="heading-primary--sub">is where life happens</div>
           </h1>
-          <Link to="#" className="btn btn-white">discover our tours</Link>
+          <Link to="#" className="btn btn--white btn--animated">discover our tours</Link>
         </div>
       </header>
     </div>
