cd2590196d462f94c70ea65c7c84a7194e7c4554
Luis Giraldo <luisgiraldo@Luiss-MacBook-Pro.local>
2022-06-23 15:48:45 -0400

    Added sass

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,11 @@
-import React from 'react';
-import logo from './img/logo-white.png';
-import './App.css';
-import { Link } from "react-router-dom";
+import React, { ReactElement } from "react";
+import "./App.scss";
+import Header from "./components/header/Header";
 
-function App() {
+function App(): ReactElement {
   return (
     <div className="App">
-      <header className="header">
-        <div className="header__logo-box">
-          <img src={logo} alt="logo" className="header__logo" />
-        </div>
-        <div className="header__text-box">
-          <h1 className="heading-primary">
-            <div className="heading-primary--main">outdoors</div>
-            <div className="heading-primary--sub">is where life happens</div>
-          </h1>
-          <Link to="#" className="btn btn--white btn--animated">discover our tours</Link>
-        </div>
-      </header>
+      <Header />
     </div>
   );
 }
